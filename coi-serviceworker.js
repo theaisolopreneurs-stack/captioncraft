f56bf917e9f5591f1cb23bba15e2575fbfb66f57
theaisolopreneurs-stack <the.aisolopreneurs@gmail.com>
2026-07-12 18:14:03 -0500

    Add coi-serviceworker.js

---
+++ coi-serviceworker.js
@@ -0,0 +1,20 @@
+/* coi-serviceworker v0.1.7 - Mislav Marohnić - https://github.com/gzuidhof/coi-serviceworker */
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener("fetch", (event) => {
+  if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") return;
+  event.respondWith(
+    fetch(event.request).then((response) => {
+      if (response.status === 0) return response;
+      const newHeaders = new Headers(response.headers);
+      newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+      newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+      newHeaders.set("Cross-Origin-Resource-Policy", "cross-origin");
+      return new Response(response.body, {
+        status: response.status,
+        statusText: response.statusText,
+        headers: newHeaders,
+      });
+    })
+  );
+});
